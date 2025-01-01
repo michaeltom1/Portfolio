3eb63f179a019c3ec1d9b4eab19d94b3036cbea4
michaeltom1 <mic81070@gmail.com>
2025-01-01 20:00:35 +0100

    modified about me description

--- src/components/sections/about.jsx
+++ src/components/sections/about.jsx
@@ -21,9 +21,9 @@ export default function About() {
             >
               Hi I&#39;m Michael, a passionate web developer. I have always been
               interested in technology and computers. My journey began in 2023
-              when I discovered Python, which sparked my transition into
-              full-stack web development. Ever since then, I have been deeply
-              involved in building websites.
+              when I discovered Python, which sparked my transition into web
+              development. Ever since then, I have been deeply involved in
+              building websites.
               <br />
               <br />I combine technical expertise with creative problem-solving
               to build responsive, user-centric websites that make an impact.
